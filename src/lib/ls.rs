ed865fac12d3ecb3241741899c43b41da0b345e4
jliu2179 <jliu2179@gmail.com>
2019-03-24 02:58:54 -0400

    add finished instruction struct

--- src/lib/ls.rs
+++ src/lib/ls.rs
@@ -1,4 +1,11 @@
 use std::fs;
+use std::io;
+use std::io::Write;
+use std::fs::DirEntry;
+use std::os::unix::fs::MetadataExt;
+//use std::os::unix::fs::DirEntryExt;
+use std::os::unix::fs::PermissionsExt;
+
 use std::collections::HashSet;
 
 use crate::lib::lexeme::Lexeme;
@@ -13,7 +20,7 @@ enum ContentOption {
 #[derive(Debug)]
 enum FormatOption {
     List,
-    LongList(ListContent),
+    LongList,
     NoList,
 }
 
@@ -30,6 +37,7 @@ struct ListContent {
 enum NameOption {
     Normal,
     DoubleQuotes,
+    AppendSlash,
 }
 
 #[derive(Debug)]
@@ -38,86 +46,93 @@ enum SortOption {
     Size,
     Time,
     Order,
-    ReverseAlphabetical,
-    ReverseSize,
-    ReverseTime,
-    ReverseOrder,
+    Extension,
 }
 
 #[derive(Debug)]
 struct Instruction {
     valid: bool,
+    inode: bool,
+    reverse_sort: bool,
     format: FormatOption,
     content: ContentOption,
     name: NameOption,
     sort: SortOption,
+    list_content: ListContent,
 }
 
 pub fn ls(lexemes: Vec<Lexeme>) {
     println!("\nExecuting from module 'ls'. . .");
 
-
     let mut options: HashSet<String> = HashSet::new();
     let mut files: Vec<&str> = Vec::new();
+    
+    let mut instruction = Instruction {
+        valid: true,
+        inode: false,
+        reverse_sort: false,
+        format: FormatOption::NoList,
+        content: ContentOption::None,
+        name: NameOption::Normal,
+        sort: SortOption::Alphabetical,
+        list_content: 
+            ListContent {
+                author: false,
+                owner: true,
+                group: true,
+                human_readable: false,
+                size: true,
+            },
+    };
 
-    if lexemes.len() > 0 {
-        
-        // separate option and files
-        for lexeme in &lexemes {
-            match &lexeme {
-                Lexeme::OPTION(_option) => {
-                    
-                    if !lexeme.unwrap().starts_with("--") {
-                        println!("OPTION: {}", &lexeme.unwrap());
-                        options.extend(parse_options(&lexeme.unwrap()));
-                    } else {
-                        options.insert(lexeme.unwrap().to_string());
-                    }
-                },
-
-                Lexeme::FILE(_file) => {
-                    files.push(&lexeme.unwrap());
-                    println!("FILE: {}", &lexeme.unwrap());
-                },
-            }
-        }
-        
-        println!("\nOptions vec:");
-        for option in &options {
-            println!("{}", &option);
-        }
-        
-        println!("\nFiles vec:");
-        for file in &files {
-            println!("{}", &file);
-        }
-
-        // process options 
-        
-        let instruction = process_options(options);
-
-        // display based on options
-        display(&instruction);
-
-
-    } else {
-        ls_noargs();
+    if lexemes.len() == 0 {
+        files.push(".");
+        let output = process_instruction(&mut instruction, files);
+        display(output.unwrap());
+        return;
     }
-}
 
-fn ls_noargs() {
-    println!("ls no args function called!");
-
-    if let Ok(entries) = fs::read_dir(".") {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if !entry.file_name().to_str().map(|s| s.starts_with(".")).unwrap() {
-                    print!("{}   ", entry.file_name().to_str().unwrap());
+    // separate option and files
+    for lexeme in &lexemes {
+        match &lexeme {
+            Lexeme::OPTION(_option) => {
+                
+                if !lexeme.unwrap().starts_with("--") {
+                    println!("OPTION: {}", &lexeme.unwrap());
+                    options.extend(parse_options(&lexeme.unwrap()));
+                } else {
+                    options.insert(lexeme.unwrap().to_string());
                 }
-            }
+            },
+
+            Lexeme::FILE(_file) => {
+                files.push(&lexeme.unwrap());
+                println!("FILE: {}", &lexeme.unwrap());
+            },
         }
     }
-    println!();
+    
+    println!("\nOptions vec:");
+    for option in &options {
+        println!("{}", &option);
+    }
+    
+    println!("\nFiles vec:");
+    for file in &files {
+        println!("{}", &file);
+    }
+
+    if files.is_empty() {
+        files.push(".");
+    }
+
+    // process options 
+    process_options(&mut instruction, options);
+
+    // process instruction
+    process_instruction(&mut instruction, files);
+
+    // display based on options
 }
 
 
@@ -138,7 +153,6 @@ fn parse_options(options: &str) -> HashSet<String> {
     let char_set: Vec<char> = options.chars().collect();
     
     for op in &char_set[1..] {
-        //println!("{}", op);
         option_set.insert("-".to_owned() + &op.to_string());
     }
 
@@ -151,91 +165,101 @@ fn parse_options(options: &str) -> HashSet<String> {
 /// 
 /// 
 /// 
-fn process_options(options: HashSet<String>) -> Instruction {
+fn process_options(instruction: &mut Instruction, options: HashSet<String>) {
     println!("\nPrinting from function process_options");
-    let mut instruction = Instruction {
-        valid: true,
-        format: FormatOption::NoList,
-        content: ContentOption::None,
-        name: NameOption::Normal,
-        sort: SortOption::Alphabetical,
-    };
+
 
     for option in &options {
         match option.as_ref() {
             "-a" | "--all" => {
                 println!("{} confirmed", option);
+                instruction.content = ContentOption::All;
             },
 
             "-A" | "--almost-all" => {
                 println!("{} confirmed", option);
+                instruction.content = ContentOption::AlmostAll;
             },
 
             "--author" => {
                 println!("{} confirmed", option);
+                instruction.list_content.author = true;
             },
             
             "-g" => {
                 println!("{} confirmed", option);
-            },
-            
-            "--group-directories-first" => {
-                println!("{} confirmed", option);
+                instruction.list_content.owner = false;
+                instruction.format = FormatOption::LongList;
             },
 
             "-G" | "--no-group" => {
                 println!("{} confirmed", option);
+                instruction.list_content.group = false;
             },
-
 
             "-h" | "--human-readable" => {
                 println!("{} confirmed", option);
+                instruction.list_content.human_readable = true;
             },
 
+            "-i" | "--inode" => {
+                println!("{} confirmed", option);
+                instruction.inode = true;
+            }
+
             "-l" => {
-                println!("{} confirmed", option);  
+                println!("{} confirmed", option);
+                instruction.format = FormatOption::LongList;
             },
 
             "-o" => {
                 println!("{} confirmed", option);
+                instruction.list_content.group = false;
             },
-
+            
             "-p" | "--indicator-style=slash" => {
                 println!("{} confirmed", option);
+                instruction.name = NameOption::AppendSlash;
             },
             
             "-Q" | "--quote-name" => {
                 println!("{} confirmed", option);
+                instruction.name = NameOption::DoubleQuotes;
             },
             
             "-r" | "--reverse" => {
                 println!("{} confirmed", option);
+                instruction.reverse_sort = true;
             },
             
             "-s" | "--size" => {
                 println!("{} confirmed", option);
+                instruction.list_content.size = true; 
             },
             
             "-S" => {
                 println!("{} confirmed", option);
+                instruction.sort = SortOption::Size;
             },
             
             "-t" => {
                 println!("{} confirmed", option);
+                instruction.sort = SortOption::Time;
             },
             
             "-U" => {
                 println!("{} confirmed", option);
+                instruction.sort = SortOption::Order;
             },
             
             "-X" => {
                 println!("{} confirmed", option);
+                instruction.sort = SortOption::Extension;
             },
             
             "-1" => {
-                instruction.format = FormatOption::List;
                 println!("{} confirmed", option);
-
+                instruction.format = FormatOption::List;
             },
 
             _ => {
@@ -245,11 +269,84 @@ fn process_options(options: HashSet<String>) -> Instruction {
             }
         }
     }
-
-    return instruction
 }
 
-fn display(instruction: &Instruction) {
+fn process_instruction(instruction: &mut Instruction, files: Vec<&str>) -> io::Result<Vec<String>>{
+    println!("Printing from process_instruction: ");
+    let mut output: Vec<String> = Vec::new();
+
+    if !instruction.valid {
+        output.push("Invalid input".to_string());
+        return Ok(output)
+    }
+    
     println!("{:?}", instruction);
+    for file in files {
+        for entry in fs::read_dir(file)? {
+            if let Ok(entry) = entry {
+                match instruction.format {
+
+                    FormatOption::NoList => {
+                        match_content(instruction, &mut output, &entry);
+                    },
+
+                    FormatOption::List => {
+                        println!("List reached");
+                    },
+
+                    FormatOption::LongList => {
+                        println!("LongList");
+
+                    },
+                }
+            }
+        }
+    }
+    
+    println!("Contents of output: ");
+    for line in &output {
+        println!("{}", line);
+    }
+
+    Ok(output)
 }
 
+
+fn match_content(instruction: &Instruction, output: &mut Vec<String>, entry: &DirEntry)  {
+
+    let file_name = entry.file_name();
+    match instruction.content {
+        ContentOption::None => {
+            if !file_name.to_str().map(|s| s.starts_with(".")).unwrap() {
+                output.push(format!("{}", file_name.to_str().unwrap()));
+            }
+        },
+    
+        ContentOption::All => {
+            output.push(format!("{}", file_name.to_str().unwrap()));
+        },
+    
+        ContentOption::AlmostAll => {
+            if file_name.to_str().unwrap() != ".." || file_name.to_str().unwrap() != "." {
+                output.push(format!("{}", file_name.to_str().unwrap()));
+            }
+        },
+    }
+}
+
+
+
+
+
+
+
+
+
+fn display(output: Vec<String>) {
+    println!("Printing from display: ");
+    for line in output {
+        print!("{}", line);
+        io::stdout().flush()
+            .expect("Failed to flush stdout buffer");
+    }
+}
